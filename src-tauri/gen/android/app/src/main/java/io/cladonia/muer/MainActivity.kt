7015330e68a5f059cc4a197f4f74269430da0695
水化 <168127639+suisanka@users.noreply.github.com>
2025-07-27 05:55:34 +0800

    tauri

---
+++ src-tauri/gen/android/app/src/main/java/io/cladonia/muer/MainActivity.kt
@@ -0,0 +1,11 @@
+package io.cladonia.muer
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
